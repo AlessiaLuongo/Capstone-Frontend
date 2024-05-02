15b99451d90067b4a06290378e2c6442fd7cfe11
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-05-02 18:55:24 +0200

    Navbar layout okey

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 import "./App.css";
+import Homepage from "./components/Homepage";
+import NavbarComponent from "./components/NavbarComponent";
 
 function App() {
-  return console.log("CIAO");
+  return (
+    <div className="App">
+      <NavbarComponent />
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,13 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+const WelcomePage = () => {
+  return (
+    <Container>
+      <Row>
+        <Col xs={12} md={10} lg={10}></Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default WelcomePage;
