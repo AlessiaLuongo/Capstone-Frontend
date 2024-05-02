2c186e015507149ebcad285add70db64304468bc
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-05-02 16:55:06 +0200

    impostazioni di base okey

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import "./App.css";
+
+function App() {
+  return console.log("CIAO");
+}
+
+export default App;
